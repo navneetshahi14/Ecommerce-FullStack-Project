c15e3b8347af5942ca73b43b2bf65ddb2d83bbc3
Navneet Shahi <138277449+navneetshahi14@users.noreply.github.com>
2024-09-23 20:03:54 +0530

    Update db.js

--- ecommbackend/db/db.js
+++ ecommbackend/db/db.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose')
 // const MongooseUri = "mongodb://0.0.0.0:27017/ecomapp"
 // P1pSutPNiRngj468
-// const MongooseUri = ``
+const MongooseUri = `mongodb+srv://navneetshahi2004:P1pSutPNiRngj468@cluster123.uy6np.mongodb.net/?retryWrites=true&w=majority&appName=Cluster123`
 
 const mongooseConnect=()=>{
     // console.log(process.env.MONGOOSEURI)
-    mongoose.connect(process.env.MONGOOSEURI)
+    mongoose.connect(MongooseUri)
     let db = mongoose.connection
     db.on('error',()=>console.log("Some Error in connecting database"))
     db.once('open',()=>console.log("Database connected"))
