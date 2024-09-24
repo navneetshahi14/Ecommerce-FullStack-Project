d6e9157bfe17f75c840b3f64700ae4116c88ad59
Navneet Shahi <138277449+navneetshahi14@users.noreply.github.com>
2024-09-24 06:25:44 +0530

    Update db.js

--- ecommbackend/db/db.js
+++ ecommbackend/db/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 // const MongooseUri = "mongodb://0.0.0.0:27017/ecomapp"
 // P1pSutPNiRngj468
-const MongooseUri = `mongodb+srv://navneetshahi2004:P1pSutPNiRngj468@cluster123.uy6np.mongodb.net/?retryWrites=true&w=majority&appName=Cluster123`
+const MongooseUri = `mongodb+srv://user2004:hPGJyQO8YMATqLVJ@cluster123.uy6np.mongodb.net/?retryWrites=true&w=majority&appName=Cluster123`
 
 const mongooseConnect=async()=>{
     // console.log(process.env.MONGOOSEURI)
